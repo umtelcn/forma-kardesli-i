32181253c454dc00de26347558ecd56149550e0c
umtelcn <umtelcn@icloud.com>
2025-08-09 17:15:21 +0300

    Update IbanPage.tsx

--- components/IbanPage.tsx
+++ components/IbanPage.tsx
@@ -147,9 +147,8 @@ export default function IbanPage({
                 <div className="text-sm text-blue-800">
                   <p className="font-medium mb-1">Bilgilendirme</p>
                   <p>
-                    Havale/EFT işleminizde alıcı adı kısmına {'"Çocuklar Üşümesin"'}
-                    yazmanız yeterlidir. Açıklama kısmına bağış yaptığınız takım
-                    adını belirtebilirsiniz.
+                    Alıcı adı kısmına {'"Çocuklar Üşümesin"'}
+                    yazmanız yeterlidir. 
                   </p>
                 </div>
               </div>
